7530025cb30a307143d41dd218d09d4013ae4cd2
Daw <Daw@DESKTOP-8OVFM6T.(none)>
2023-05-11 18:05:32 +0200

    Commit 2

---
+++ ProyectoGitExamen/src/paqueteGitExamen/ClaseGit2.java
@@ -0,0 +1,10 @@
+package paqueteGitExamen;
+
+public class ClaseGit2 {
+
+	public static void main(String[] args) {
+		System.out.println("Este es un mensaje de prueba");
+		System.out.println("Esto son dos mensajes de prueba");
+	}
+	
+}
